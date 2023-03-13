0aeab9c28903f623199fd6a7c21c8e68bc17df80
최지성 <thsrhzn4@naver.com>
2023-03-13 14:53:22 +0900

    feat: 초기 데이터 설정
    각 테이블에 초기 데이터 생성

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO user values (1, "이메일", "닉네임", "비밀번호");
+INSERT INTO cultural_property values (1, "주소", "설명", "히든 설명", "이미지", 35.2053048, 126.8117677, "중꿔 네임", "한국 이름", "타입");
+INSERT INTO diary values (1, "2023-03-13 12:00:00", "사진", "리뷰", 1, 1);
+INSERT INTO star values (1, 1, 1, 0, 1, 1);
+INSERT INTO achievement values (1, "업적 이름", "캐릭터 이미지", "업적 설명", 10);
+INSERT INTO cultural_property_achievement values(1, 1, 1);
+INSERT INTO user_achievement values (1, 1, 1);
