63711797f36e311f25a5ef9da450958b73fa2e95
yangDM9378 <coco1622555@gmail.com>
2023-03-15 01:10:29 +0900

    feat: map 라이브러리 변경

--- frontend/src/components/category/CategoryMap.tsx
+++ frontend/src/components/category/CategoryMap.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable consistent-return */
 // /* eslint-disable @typescript-eslint/no-explicit-any */
 // import React, { useState, useEffect } from 'react';
 // import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
@@ -26,7 +25,6 @@
 //     async function fetchGeojson() {
 //       const response = await fetch('/korea.json');
 //       const data: Geojson = await response.json();
-//       console.log(data);
 //       setKoreaGeojson(data);
 //     }
 //     fetchGeojson();
@@ -38,10 +36,9 @@
 
 //   return (
 //     <ComposableMap
-//       projection="geoAzimuthalEqualArea"
+//       projection="geoMercator"
 //       projectionConfig={{
-//         rotate: [0.0, -53.0, 0],
-//         scale: 0,
+//         scale: 4500,
 //       }}
 //     >
 //       <Geographies geography={koreaGeojson.features}>
@@ -62,42 +59,126 @@
 //   );
 // }
 
-import { SimpleSouthKoreaMapChart } from 'react-simple-south-korea-map-chart';
+import './styles.css';
+import { DataMapsWrapper } from 'react-typescript-datamaps';
+import d3 from 'd3';
 
-const data = [
-  { locale: '부산광역시', count: 1500 },
-  { locale: '대구광역시', count: 3000 },
-  { locale: '대전광역시', count: 400 },
-  { locale: '강원도', count: 2500 },
-  { locale: '광주광역시', count: 1000 },
-  { locale: '경기도', count: 4000 },
-  { locale: '인천광역시', count: 2200 },
-  { locale: '제주특별자치도', count: 100 },
-  { locale: '충청북도', count: 49 },
-  { locale: '경상북도', count: 2000 },
-  { locale: '전라북도', count: 3300 },
-  { locale: '세종특별자치시', count: 110 },
-  { locale: '충청남도', count: 10 },
-  { locale: '경상남도', count: 0 },
-  { locale: '전라남도', count: 250 },
-  { locale: '울산광역시', count: 100 },
-  { locale: '서울특별시', count: 10000 },
+const demoProps = {
+  scope: 'india',
+  geographyConfig: {
+    popupOnHover: true,
+    highlightOnHover: true,
+    borderColor: '#444',
+    borderWidth: 0.5,
+    dataUrl:
+      'https://rawgit.com/Anujarya300/bubble_maps/master/data/geography-data/india.topo.json',
+  },
+  bubblesConfig: {
+    borderWidth: 2,
+    borderOpacity: 1,
+    borderColor: '#FFFFFF',
+    popupOnHover: true, // True to show the popup while hovering
+    radius: null,
+    popupTemplate: function (geo, data) {
+      return `<div class="hoverinfo">city: ${data.state}, Slums: ${data.radius}%</div>`;
+    },
+    fillOpacity: 0.75,
+    animate: true,
+    highlightOnHover: true,
+    highlightFillColor: '#FC8D59',
+    highlightBorderColor: 'rgba(250, 15, 160, 0.2)',
+    highlightBorderWidth: 2,
+    highlightBorderOpacity: 1,
+    highlightFillOpacity: 0.85,
+    exitDelay: 100, // Milliseconds
+    key: JSON.stringify,
+  },
+  fills: {
+    MAJOR: '#306596',
+    MEDIUM: '#0fa0fa',
+    MINOR: '#bada55',
+    defaultFill: '#dddddd',
+  },
+  data: {
+    JH: { fillKey: 'MINOR' },
+    MH: { fillKey: 'MINOR' },
+  },
+  setProjection: function (element) {
+    var projection = d3.geo
+      .mercator()
+      .center([80, 25])
+      .scale(600)
+      .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+    var path = d3.geo.path().projection(projection);
+    return { path: path, projection: projection };
+  },
+};
+
+const bubblesDemo = [
+  {
+    centered: 'MH',
+    fillKey: 'MAJOR',
+    radius: 20,
+    state: 'Maharastra',
+  },
+  {
+    centered: 'AP',
+    fillKey: 'MAJOR',
+    radius: 22,
+    state: 'Andhra Pradesh',
+  },
+  {
+    centered: 'TN',
+    fillKey: 'MAJOR',
+    radius: 16,
+    state: 'Tamil Nadu',
+  },
+  {
+    centered: 'WB',
+    fillKey: 'MEDIUM',
+    radius: 15,
+    state: 'West Bengal',
+  },
+  {
+    centered: 'MP',
+    fillKey: 'MEDIUM',
+    radius: 15,
+    state: 'Madhya Pradesh',
+  },
+  {
+    centered: 'UP',
+    fillKey: 'MINOR',
+    radius: 8,
+    state: 'Uttar Pradesh',
+  },
+  {
+    centered: 'RJ',
+    fillKey: 'MINOR',
+    radius: 7,
+    state: 'Rajasthan',
+  },
 ];
 
-export default function CategoryMap() {
-  const setColorByCount = (count: number) => {
-    if (count === 0) return '#F1F1F1';
-    if (count > 5000) return '#79D3C4';
-    if (count > 3000) return '#43cdb6';
-    if (count > 1000) return '#61CDBB';
-    if (count > 200) return '#91D9CD';
-    if (count > 100) return '#A9DFD6';
-    if (count > 50) return '#C1E5DF';
-    if (count > 5) return '#D9EBE8';
-    if (count < 5) return '#ebfffd';
-  };
-
+function Demo() {
+  const [bubbles, setBubbles] = React.useState([]);
+  React.useEffect(() => {
+    setInterval(() => {
+      setBubbles(bubblesDemo);
+    }, 1000);
+  }, []);
   return (
-    <SimpleSouthKoreaMapChart setColorByCount={setColorByCount} data={data} />
+    <div style={{ width: '600px', height: '600px' }} className="App">
+      <DataMapsWrapper {...demoProps} bubbles={bubbles} />
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <Demo />
+      </div>
+    </div>
   );
 }
