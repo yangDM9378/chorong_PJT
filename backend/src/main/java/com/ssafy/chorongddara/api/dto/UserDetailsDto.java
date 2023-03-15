383842e5bbfea711373321c17d516c2995f0c049
송해찬 <hcsong95@naver.com>
2023-03-15 17:21:50 +0900

    feat: add security config

---
+++ backend/src/main/java/com/ssafy/chorongddara/api/dto/UserDetailsDto.java
@@ -0,0 +1,54 @@
+package com.ssafy.chorongddara.api.dto;
+
+import com.ssafy.chorongddara.db.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Slf4j
+@Getter
+@AllArgsConstructor
+public class UserDetailsDto implements UserDetails {
+
+    private User user;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() { return user.getPassword(); }
+
+    // userId 자리라서 email 씀
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
--- backend/src/main/java/com/ssafy/chorongddara/api/service/UserService.java
+++ backend/src/main/java/com/ssafy/chorongddara/api/service/UserService.java
@@ -1,5 +1,11 @@
 package com.ssafy.chorongddara.api.service;
 
 
+import com.ssafy.chorongddara.api.request.UserJoinReq;
+import com.ssafy.chorongddara.db.entity.User;
+
 public interface UserService {
+    void join(UserJoinReq userJoinReq);
+    User searchUserByEmail(String email);
+
 }
