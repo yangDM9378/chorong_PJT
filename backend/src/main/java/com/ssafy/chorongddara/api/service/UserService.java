2c5aa01a6fb849e3f4df122bb93cdecc39c719e0
송해찬 <hcsong95@naver.com>
2023-03-17 17:18:51 +0900

    feat: 유저 회원정보 조회, 수정

--- backend/src/main/java/com/ssafy/chorongddara/api/service/UserService.java
+++ backend/src/main/java/com/ssafy/chorongddara/api/service/UserService.java
@@ -2,6 +2,7 @@ package com.ssafy.chorongddara.api.service;
 
 
 import com.ssafy.chorongddara.api.request.UserJoinReq;
+import com.ssafy.chorongddara.api.request.UserUpdateReq;
 import com.ssafy.chorongddara.db.entity.User;
 
 import java.util.Optional;
@@ -9,5 +10,6 @@ import java.util.Optional;
 public interface UserService {
     void join(UserJoinReq userJoinReq);
     Optional<User> getUserByEmail(String email);
+    int updateUser(String email, UserUpdateReq userUpdateReq);
 //    Optional<User> login(String email);
 }
