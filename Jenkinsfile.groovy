98370e680a60a60daba76a40ad817becac8735fc
최지성 <thsrhzn4@naver.com>
2023-03-18 18:13:53 +0900

    chore: 배포용 pipeline 추가

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,5 +7,34 @@ pipeline {
                 echo 'Testing..'
             }
         }
+        stage('Deploy') {
+            steps {
+                sshPublisher(
+                    publishers: [
+                        sshPublisherDesc(
+                            configName: 'chorongddaraDeploy',
+                            transfers: [
+                                sshTransfer(
+                                    cleanRemote: false,
+                                    excludes: '',
+                                    execCommand: 'docker-compose up --build -d',
+                                    execTimeout: 120000,
+                                    flatten: false,
+                                    makeEmptyDirs: false,
+                                    noDefaultExcludes: false,
+                                    patternSeparator: '[, ]+',
+                                    remoteDirectory: '',
+                                    remoteDirectorySDF: false,
+                                    removePrefix: '',
+                                    sourceFiles: '/README.md')
+                            ],
+                            usePromotionTimestamp: false,
+                            useWorkspaceInPromotion: false,
+                            verbose: false
+                        )
+                    ]
+                )
+            }
+        }
     }
 }
