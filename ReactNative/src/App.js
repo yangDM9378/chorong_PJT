83497293e1ccc451e3f03bfa5497ce80df696afc
cyang <cyellowgreen@gmail.com>
2023-03-20 16:38:16 +0900

    feat: 리액트 네이티브 구조변경

---
+++ ReactNative/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>최유경 바보입니다.</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 30,
+  },
+});
+
+export default App;
