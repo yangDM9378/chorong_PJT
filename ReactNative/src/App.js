a6007cba1ebd3a533a843052c9394cc5f061a93b
cyang <cyellowgreen@gmail.com>
2023-03-20 17:03:55 +0900

    feat: native app에 gps 연동
    Former-commit-id: 11b91ade274295b05860f457cada8b529a1faba4

--- ReactNative/src/App.js
+++ ReactNative/src/App.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {PermissionsAndroid} from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
 
 const App = () => {
+  const [location, setLocation] = useState(undefined);
+
+  useEffect(() => {
+    PermissionsAndroid.requestMultiple([
+      PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    ]);
+    const _watchId = Geolocation.watchPosition(
+      position => {
+        const {latitude, longitude} = position.coords;
+        setLocation({latitude, longitude});
+      },
+      error => {
+        console.log(error);
+      },
+      {
+        enableHighAccuracy: true,
+        distanceFilter: 0,
+        interval: 5000,
+        fastestInterval: 2000,
+      },
+    );
+
+    return () => {
+      if (_watchId) {
+        Geolocation.clearWatch(_watchId);
+      }
+    };
+  }, []);
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>최유경 바보입니다.</Text>
+      {location ? (
+        <>
+          <Text> latitude: {location.latitude} </Text>
+          <Text> longitude: {location.longitude} </Text>
+        </>
+      ) : (
+        <></>
+      )}
     </View>
   );
 };
