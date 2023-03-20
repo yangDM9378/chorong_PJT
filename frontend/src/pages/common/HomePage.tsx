157f83b1fdc46e29917f6fbc96d4b470cc116b7c
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2023-03-20 16:17:20 +0900

    feat: tailwind 글자체 및 색상 지정

--- frontend/src/pages/common/HomePage.tsx
+++ frontend/src/pages/common/HomePage.tsx
@@ -1,3 +1,8 @@
 export default function HomePage() {
-  return <div>문구111111</div>;
+  return (
+    <div className="font-nanum">
+      <p className="text-mainred">문구</p>
+      <p className="text-maingreen">111111</p>
+    </div>
+  );
 }
