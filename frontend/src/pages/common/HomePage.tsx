2835c757fffd11a8d5a72d3725d84eaa047497a4
Whitepaperone <qorwldnjs100@gmail.com>
2023-03-21 17:45:41 +0900

    feat: account 모달 띄우기 & HomeCarousel 기능 구현

--- frontend/src/pages/common/HomePage.tsx
+++ frontend/src/pages/common/HomePage.tsx
@@ -1,8 +1,52 @@
+import ReactModal from 'react-modal';
+import { useState } from 'react';
+import SignInModal from '../../components/common/home/SignInModal';
+import SignUpModal from '../../components/common/home/SignUpModal';
+import HomeCarousel from '../../components/common/home/HomeCarousel';
+
 export default function HomePage() {
+  const [signUpOpen, setSignUpOpen] = useState(false);
+  const [signInOpen, setSignInOpen] = useState(false);
+
+  const signUpClose = () => {
+    setSignUpOpen(false);
+  };
+  const signInClose = () => {
+    setSignInOpen(false);
+  };
+
   return (
     <div className="font-nanum">
       <p className="text-mainred">문구</p>
       <p className="text-maingreen">111111</p>
+      <HomeCarousel />
+      <button
+        type="button"
+        onClick={() => {
+          setSignInOpen(true);
+        }}
+      >
+        Sign In
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          setSignUpOpen(true);
+        }}
+      >
+        Sign Up
+      </button>
+
+      {signInOpen && (
+        <ReactModal isOpen={signInOpen} onRequestClose={signInClose}>
+          <SignInModal onClose={signInClose} />
+        </ReactModal>
+      )}
+      {signUpOpen && (
+        <ReactModal isOpen={signUpOpen} onRequestClose={signUpClose}>
+          <SignUpModal onClose={signUpClose} />
+        </ReactModal>
+      )}
     </div>
   );
 }
