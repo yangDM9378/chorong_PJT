f5239aadba3862c88a905ff8216bda1fe4bf1ba4
cyang <cyellowgreen@gmail.com>
2023-03-21 17:17:29 +0900

    feat: RN 환경 설정 폴더 업로드

---
+++ RN/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from "react-native";
+
+
+const App = () =>{
+    return (
+        <View style={{ flex: 1 }}>
+          <Text style={{padding: 10}}>Hello</Text>
+        </View>
+      );
+}
+
+
+
+export default App;
