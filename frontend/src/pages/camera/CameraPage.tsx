34e9fb9646ce545b2bc2e0793e7cdc4cb9546f98
Whitepaperone <qorwldnjs100@gmail.com>
2023-03-22 11:55:51 +0900

    feat: camera 테스트

--- frontend/src/pages/camera/CameraPage.tsx
+++ frontend/src/pages/camera/CameraPage.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-// import TeachableMachine from '../../components/camera/TeachableMachine';
+import TeachableMachine from '../../components/camera/TeachableMachine';
 import VideoPoseModel from '../../components/camera/VideoPoseModel';
 
 export default function CameraPage() {
   return (
     <div>
       <VideoPoseModel />
-      {/* <TeachableMachine /> */}
+      <TeachableMachine />
     </div>
   );
 }
