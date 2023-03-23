4fe86dfa4cca133096bb03eaadb643888712aceb
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2023-03-23 14:03:42 +0900

    feat: main modal 및 CSS 정용

--- frontend/src/components/common/home/SignInModal.tsx
+++ frontend/src/components/common/home/SignInModal.tsx
@@ -1,7 +1,42 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import Modal from 'react-modal';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import styled from 'styled-components';
 import tw from 'twin.macro';
+
+type ModalProps = {
+  isOpen: boolean;
+  close: () => void;
+};
+
+type FormValues = {
+  email: string;
+  password: string;
+};
+
+export default function SignInModal({ isOpen, close }: ModalProps) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
+  const onSubmit: SubmitHandler<FormValues> = () => {
+    close();
+  };
+  return (
+    <Modal isOpen={isOpen} onRequestClose={() => close()} ariaHideApp={false}>
+      <S.SignInForm onSubmit={handleSubmit(onSubmit)}>
+        <S.Input type="email" {...register('email', { required: true })} />
+        {errors.email && errors.email.type === 'required' && (
+          <div>이메일을 입력해 주세요!</div>
+        )}
+        <S.Input type="password" {...register('password')} />
+
+        <S.Input type="submit" />
+      </S.SignInForm>
+    </Modal>
+  );
+}
 
 const S = {
   SignInForm: styled.form`
@@ -11,35 +46,3 @@ const S = {
     ${tw`block`}
   `,
 };
-
-type SignInModalProps = {
-  onClose: () => void;
-};
-
-type FormValues = {
-  email: string;
-  password: string;
-};
-
-export default function SignInModal({ onClose }: SignInModalProps) {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormValues>();
-  const onSubmit: SubmitHandler<FormValues> = () => {
-    onClose();
-  };
-
-  return (
-    <S.SignInForm onSubmit={handleSubmit(onSubmit)}>
-      <S.Input type="email" {...register('email', { required: true })} />
-      {errors.email && errors.email.type === 'required' && (
-        <div>이메일을 입력해 주세요!</div>
-      )}
-      <S.Input type="password" {...register('password')} />
-
-      <S.Input type="submit" />
-    </S.SignInForm>
-  );
-}
