e78b2661172a4d578b778f50c3e9e7618b555fe3
Lucas0105 <dnjswo105@gmail.com>
2023-03-23 19:44:19 +0900

    feat: Webview 페이지

--- AndroidStudio/app/src/main/java/com/example/androidstudio/WebviewContainer.kt
+++ AndroidStudio/app/src/main/java/com/example/androidstudio/WebviewContainer.kt
@@ -1,11 +1,30 @@
 package com.example.androidstudio
 
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class WebviewContainer : AppCompatActivity() {
+    private lateinit var webview: WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_webview_container)
+
+        webview = findViewById(R.id.webview)
+
+        webview.apply {
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+        }
+
+        webview.loadUrl("https://j8c101.p.ssafy.io/")
+    }
+
+    override fun onBackPressed() {
+        webview = findViewById(R.id.webview)
+
+        if (webview.canGoBack()) { webview.goBack() }
+        else { finish() }
     }
 }
