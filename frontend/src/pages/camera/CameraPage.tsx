0707baa5624a22ede1c8ffb74515a0f353d8d8c6
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2023-03-24 09:46:23 +0900

    feat: CSS 수정

--- frontend/src/pages/camera/CameraPage.tsx
+++ frontend/src/pages/camera/CameraPage.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import TeachableMachine from '../../components/camera/TeachableMachine';
-// import VideoPoseModel from '../../components/camera/VideoPoseModel';
+import VideoPoseModel from '../../components/camera/VideoPoseModel';
 
 export default function CameraPage() {
   return (
     <div>
-      {/* <VideoPoseModel /> */}
+      <VideoPoseModel />
       <TeachableMachine />
     </div>
   );
