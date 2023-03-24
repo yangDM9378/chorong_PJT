323c3dfbd31861562b1c69089d93558ef85f59a6
최지성 <thsrhzn4@naver.com>
2023-03-24 15:57:39 +0900

    feat: 갤러리 생성

---
+++ backend/src/main/java/com/ssafy/chorongddara/api/request/GalleryInsertReq.java
@@ -0,0 +1,10 @@
+package com.ssafy.chorongddara.api.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GalleryInsertReq {
+    private Integer culturalPropertyId;
+}
