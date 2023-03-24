a87c0bbcc1b4c87a152a7f6df734b094413d5b03
최지성 <thsrhzn4@naver.com>
2023-03-24 12:06:15 +0900

    feat: cors 처리

--- backend/src/main/java/com/ssafy/chorongddara/config/WebMvcConfig.java
+++ backend/src/main/java/com/ssafy/chorongddara/config/WebMvcConfig.java
@@ -2,6 +2,9 @@ package com.ssafy.chorongddara.config;
 
 import org.springframework.context.annotation.Bean; //add
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
