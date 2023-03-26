b4e59eeb04df8c6c8dbdf6e0e421390c948577f2
yangDM9378 <coco1622555@gmail.com>
2023-03-26 16:57:24 +0900

    feat: Main(Stage) CSS

--- frontend/src/components/common/main/MainHeader.tsx
+++ frontend/src/components/common/main/MainHeader.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable no-console */
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
 import { useNavigate } from 'react-router-dom';
 import { getMe } from '../../../api/userApi';
 
@@ -13,34 +15,52 @@ export default function MainTheme() {
   const [userMe, setUserMe] = useState<User | null>(null);
 
   useEffect(() => {
-    const fetchUser = async () => {
+    const getMeData = async () => {
       const response = await getMe();
       if (response) {
         const { userId, email, nickname } = response.result;
         setUserMe({ userId, email, nickname });
       }
     };
-    fetchUser();
+    getMeData();
   }, []);
 
   // 갤러리 클릭
   const navigate = useNavigate();
   const goGallery = () => {
-    navigate('/culturalpropertydetail');
+    navigate(`/gallery/${userMe?.userId}`);
   };
 
   return (
-    <div>
+    <S.Container>
       {userMe ? (
         <div>
-          <p>Nickname: {userMe.nickname}</p>
+          <S.Nickname>{userMe.nickname}</S.Nickname>
+          <S.Email>{userMe.email}</S.Email>
         </div>
       ) : (
         <div>Loading...</div>
       )}
-      <button type="button" onClick={goGallery}>
-        Gallery
-      </button>
-    </div>
+      <S.Button>
+        <button type="button" onClick={goGallery}>
+          갤러리
+        </button>
+      </S.Button>
+    </S.Container>
   );
 }
+
+const S = {
+  Container: styled.div`
+    ${tw`flex justify-between items-center bg-white w-[90vw] h-[12vh] rounded-3xl mt-[3vh] px-[8vw]`}
+  `,
+  Nickname: styled.div`
+    ${tw`text-[3vh] font-bold`}
+  `,
+  Email: styled.div`
+    ${tw`text-[2vh]`}
+  `,
+  Button: styled.div`
+    ${tw`w-[22vw] h-[6vh] flex justify-center items-center border-[326E6C] border-[1vw] rounded-lg`}
+  `,
+};
