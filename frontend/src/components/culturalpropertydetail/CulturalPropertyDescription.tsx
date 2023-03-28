230cae90ed0a17c787fff562c53db8115d8af278
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2023-03-28 15:27:51 +0900

    feat: 서버와 통신하여 데이터 불러오기;

--- frontend/src/components/culturalpropertydetail/CulturalPropertyDescription.tsx
+++ frontend/src/components/culturalpropertydetail/CulturalPropertyDescription.tsx
@@ -1,5 +1,37 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
+import { AppState } from '../../store';
+import { CulturalPropertyData } from '../../types/culturalpropertytype';
 
 export default function CulturalPropertyDescription() {
-  return <div>CulturalPropertyDescription</div>;
+  const culturalPropertydata = useSelector<
+    AppState,
+    CulturalPropertyData | null
+  >(({ culturalProperty }) => culturalProperty.value);
+  const starAr = culturalPropertydata?.result.starCountRes.starAr
+    ? culturalPropertydata?.result.starCountRes.starAr
+    : 0;
+
+  const starPose = culturalPropertydata?.result.starCountRes.starAr
+    ? culturalPropertydata?.result.starCountRes.starPose
+    : 0;
+
+  const starQuiz = culturalPropertydata?.result.starCountRes.starQuiz
+    ? culturalPropertydata?.result.starCountRes.starQuiz
+    : 0;
+
+  const starCnt = starAr + starPose + starQuiz;
+
+  return (
+    <div>
+      <div>{culturalPropertydata?.result.culturalProperty.description}</div>
+
+      {starCnt > 0 ? (
+        <div>
+          {culturalPropertydata?.result.culturalProperty.hiddenDescription}
+        </div>
+      ) : (
+        <div>111</div>
+      )}
+    </div>
+  );
 }
--- frontend/src/components/culturalpropertydetail/CulturalPropertyHeader.tsx
+++ frontend/src/components/culturalpropertydetail/CulturalPropertyHeader.tsx
@@ -1,11 +1,43 @@
-import React from 'react';
-import Star from '../common/star/Star';
+// import styled from 'styled-components';
+// import tw from 'twin.macro';
+import { useSelector } from 'react-redux';
+import { AppState } from '../../store';
+import { CulturalPropertyData } from '../../types/culturalpropertytype';
 
 export default function CulturalPropertyHeader() {
+  const culturalPropertydata = useSelector<
+    AppState,
+    CulturalPropertyData | null
+  >(({ culturalProperty }) => culturalProperty.value);
+  const starAr = culturalPropertydata?.result.starCountRes.starAr
+    ? culturalPropertydata?.result.starCountRes.starAr
+    : 0;
+
+  const starPose = culturalPropertydata?.result.starCountRes.starAr
+    ? culturalPropertydata?.result.starCountRes.starPose
+    : 0;
+
+  const starQuiz = culturalPropertydata?.result.starCountRes.starQuiz
+    ? culturalPropertydata?.result.starCountRes.starQuiz
+    : 0;
+
+  const starCnt = starAr + starPose + starQuiz;
+
   return (
     <div>
-      <Star />
-      <div>이미지</div>
+      <div>{starCnt}</div>
+      <img
+        src={culturalPropertydata?.result.culturalProperty.image}
+        alt={culturalPropertydata?.result.culturalProperty.image}
+      />
+      <div>{culturalPropertydata?.result.culturalProperty.nameKo}</div>
+      <div>{culturalPropertydata?.result.culturalProperty.address}</div>
+      <hr />
     </div>
   );
 }
+// const S = {
+//   Container: styled.div`
+//     ${tw`relative flex items-center justify-center`}
+//   `,
+// };
