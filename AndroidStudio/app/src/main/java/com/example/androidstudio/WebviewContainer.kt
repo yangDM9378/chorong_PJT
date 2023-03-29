d5584e2d37412602b7943dd00d13a17522edcc50
Lucas0105 <dnjswo105@gmail.com>
2023-03-29 15:33:43 +0900

    feat: webview 설정 - unitylibrary 삭제 - localstorage 허용 - alert 허용
    Former-commit-id: 865f71548316b7bebfcb0ac9d3f804248b8e5327

--- AndroidStudio/app/src/main/java/com/example/androidstudio/WebviewContainer.kt
+++ AndroidStudio/app/src/main/java/com/example/androidstudio/WebviewContainer.kt
@@ -3,6 +3,7 @@ package com.example.androidstudio
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 
 class WebviewContainer : AppCompatActivity() {
@@ -16,9 +17,18 @@ class WebviewContainer : AppCompatActivity() {
         webview.apply {
             webViewClient = WebViewClient()
             settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            settings.setSupportMultipleWindows(true)
+            settings.javaScriptCanOpenWindowsAutomatically = true
+            settings.loadWithOverviewMode = true
+            settings.useWideViewPort = true
+            settings.setSupportZoom(false)
+            settings.builtInZoomControls = false
         }
 
         webview.loadUrl("https://j8c101.p.ssafy.io/")
+        webview.webViewClient = WebViewClient()
+        webview.webChromeClient = WebChromeClient()
     }
 
     override fun onBackPressed() {
