0bed793d5f951ca099a6fc75f1f6f78fec55d14d
Lucas0105 <dnjswo105@gmail.com>
2023-03-30 10:12:12 +0900

    feat: 어플 클릭시 webview 환경에서 실행
    Former-commit-id: a601beccbf0943357350fd743e2222481e81b01f

--- AndroidStudio/app/src/main/java/com/ssafy/androidstudio/MainActivity.kt
+++ AndroidStudio/app/src/main/java/com/ssafy/androidstudio/MainActivity.kt
@@ -1,53 +1,55 @@
 package com.ssafy.androidstudio
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import com.ssafy.androidstudio.recyclingtrashcans.TrashcanGeoActivity
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 //import com.unity3d.player.UnityPlayerActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var webview: WebView
+    private var backBtnTime: Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        settingBtnWebview()
-//        settingBtnGame()
-        settingBtnGps()
-        settingBtnTest()
-    }
 
-    fun settingBtnWebview() {
-        val button = findViewById<Button>(R.id.webview)
-        button.setOnClickListener{
-            val intent = Intent(this, WebviewContainer::class.java)
-            startActivity(intent)
+        webview = findViewById(R.id.webview)
+
+        webview.apply {
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            settings.setSupportMultipleWindows(true)
+            settings.javaScriptCanOpenWindowsAutomatically = true
+            settings.loadWithOverviewMode = true
+            settings.useWideViewPort = true
+            settings.setSupportZoom(false)
+            settings.builtInZoomControls = false
         }
+
+//        webview.loadUrl("https://j8c101.p.ssafy.io/")
+        webview.loadUrl("http://192.168.100.130:3000")
+
+        webview.webViewClient = WebViewClient()
+        webview.webChromeClient = WebChromeClient()
     }
 
-//    fun settingBtnGame() {
-//        val button = findViewById<Button>(R.id.game)
-//        button.setOnClickListener{
-//            val intent = Intent(this, Game::class.java)
-//            startActivity(Intent(this, UnityPlayerActivity::class.java))
-//        }
-//    }
-
-    fun settingBtnGps() {
-        val button = findViewById<Button>(R.id.gps)
-        button.setOnClickListener{
-            val intent = Intent(this, TrashcanGeoActivity::class.java)
-            startActivity(intent)
-        }
-    }
-
-    fun settingBtnTest() {
-        val button = findViewById<Button>(R.id.test)
-        button.setOnClickListener{
-            val intent = Intent(this, Test::class.java)
-            startActivity(intent)
+    override fun onBackPressed() {
+        val curTime = System.currentTimeMillis()
+        val gapTime = curTime - backBtnTime
+        if (webview.canGoBack()) {
+            webview.goBack()
+        } else if (0 <= gapTime && 2000 >= gapTime) {
+            super.onBackPressed()
+        } else {
+            backBtnTime = curTime
+            Toast.makeText(this, "한번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
         }
     }
 }
