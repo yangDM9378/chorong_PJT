aec83faab1317bf69048170102a7a1657cfd19bc
cyang <cyellowgreen@gmail.com>
2023-03-30 15:42:51 +0900

    style: 상세페이지 수정(높이, 태그)

--- frontend/src/api/galleryApi.tsx
+++ frontend/src/api/galleryApi.tsx
@@ -9,14 +9,43 @@ interface GalleryData {
   result: GalleryResult[];
 }
 
+interface SetGalleryData {
+  culturalPropertyId: number;
+  picture: File | undefined;
+}
 interface GalleryResult {
   picture: File;
+}
+
+// 개별 문화재의 photo 데이터 값입니다.
+interface PhotoData {
+  resultCode: number;
+  resultMsg: string;
+  result: [];
 }
 
 export async function getGalleryData(): Promise<GalleryData | null> {
   try {
     const response: AxiosResponse<GalleryData> = await authApi.get(
       `/galleries/`,
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+export async function setGalleryData(data: SetGalleryData): Promise<void> {
+  await authApi.post(`/galleries/`, data);
+}
+
+export async function getGalleryDetailData(
+  no?: number,
+): Promise<PhotoData | null> {
+  try {
+    const response: AxiosResponse<PhotoData> = await authApi.get(
+      `/galleries/${no}`,
     );
     return response.data;
   } catch (error) {
