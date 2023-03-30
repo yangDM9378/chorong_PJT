2b45aaf35ba71a33e9f8d7758d6cc7a62abf5488
Lucas0105 <dnjswo105@gmail.com>
2023-03-30 12:08:50 +0900

    feat: 안드로이드 스튜디오와 리엑트 통신
    Former-commit-id: 73e65ed1a0e7cb3a4a5359165ea802f7437d60bc

--- AndroidStudio/app/src/main/java/com/ssafy/androidstudio/MainActivity.kt
+++ AndroidStudio/app/src/main/java/com/ssafy/androidstudio/MainActivity.kt
@@ -1,24 +1,32 @@
 package com.ssafy.androidstudio
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.webkit.JavascriptInterface
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
-
-//import com.unity3d.player.UnityPlayerActivity
+import com.ssafy.androidstudio.recyclingtrashcans.TrashcanGeoActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var webview: WebView
     private var backBtnTime: Long = 0
 
+    inner class WebAppInterface(private val mContext: Context) {
+        @JavascriptInterface
+        fun showGame(message: String) {
+            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+            val intent = Intent(mContext, TrashcanGeoActivity::class.java)
+            mContext.startActivity(intent)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
         webview = findViewById(R.id.webview)
 
         webview.apply {
@@ -31,10 +39,12 @@ class MainActivity : AppCompatActivity() {
             settings.useWideViewPort = true
             settings.setSupportZoom(false)
             settings.builtInZoomControls = false
+            // JavaScript 인터페이스 활성화
+            addJavascriptInterface(WebAppInterface(this@MainActivity), "Android")
         }
 
-//        webview.loadUrl("https://j8c101.p.ssafy.io/")
-        webview.loadUrl("http://192.168.100.130:3000")
+        webview.loadUrl("https://j8c101.p.ssafy.io/")
+//        webview.loadUrl("http://192.168.100.130:3000")
 
         webview.webViewClient = WebViewClient()
         webview.webChromeClient = WebChromeClient()
