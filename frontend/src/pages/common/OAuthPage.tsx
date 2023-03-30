a3d66d8f25080b99197e76b3c41bea4efa87128f
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2023-03-30 15:52:24 +0900

    google Oauth 수정

--- frontend/src/pages/common/OAuthPage.tsx
+++ frontend/src/pages/common/OAuthPage.tsx
@@ -1,24 +1,19 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function OAuthPage() {
-  // const ACCESS_TOKEN = 'accessToken';
-  const [token, setToken] = useState('');
-
-  const [Error, setError] = useState('');
   const navigate = useNavigate();
 
   useEffect(() => {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    cconsole.log(urlSearchParams);
+    const token = urlSearchParams.get('token');
+    console.log(token);
     if (token) {
       localStorage.setItem('accesstoken', token);
-      setToken('token');
       navigate('/main');
     }
-    if (Error) {
-      console.log(Error);
-      navigate('/');
-    }
-  }, [token]);
+  }, []);
 
   return <div>로그인 중</div>;
 }
