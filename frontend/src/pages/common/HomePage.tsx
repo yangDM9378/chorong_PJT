095a9d35fd35ab2af1fbfd961963f446cef4d2af
cyang <cyellowgreen@gmail.com>
2023-03-31 14:49:02 +0900

    style: main 이미지
    Former-commit-id: cb9ccdd8491ebcacb770fe6d614f4e9b55242b8d

--- frontend/src/pages/common/HomePage.tsx
+++ frontend/src/pages/common/HomePage.tsx
@@ -3,7 +3,7 @@ import Home from '../../components/common/home/Home';
 export default function HomePage() {
   return (
     <div
-      className="font-nanum w-[100vw] h-[100vh]"
+      className="font-GmarketSansMedium w-[100vw] h-[100vh]"
       style={{
         backgroundImage: `url(/main/bg/bg0.png)`,
         backgroundSize: 'cover',
