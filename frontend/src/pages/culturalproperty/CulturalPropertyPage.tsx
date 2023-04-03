c17fe43d43f3bb1e39fe45dd6ba767902afc24a7
cyang <cyellowgreen@gmail.com>
2023-04-03 13:47:36 +0900

    feat: gps 확인 기능 삭제
    Former-commit-id: 4ed3d1377293b2498da87b55b849e9a5be271fd1

--- frontend/src/pages/culturalproperty/CulturalPropertyPage.tsx
+++ frontend/src/pages/culturalproperty/CulturalPropertyPage.tsx
@@ -63,7 +63,6 @@ export default function CulturalPropertyPage() {
         data?.result.culturalProperty.latitude,
         data?.result.culturalProperty.longitude,
       );
-      alert(coords.latitude);
     }
   }, [coords]);
 
