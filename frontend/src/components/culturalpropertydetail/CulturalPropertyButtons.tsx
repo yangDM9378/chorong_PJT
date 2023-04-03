bb305b2d09140556657ec031a25cf301cf0b3bec
cyang <cyellowgreen@gmail.com>
2023-04-03 18:00:07 +0900

    feat: gps 수정
    Former-commit-id: 70cb6017e0f126863cde4302b466dd7f5df9f887

--- frontend/src/components/culturalpropertydetail/CulturalPropertyButtons.tsx
+++ frontend/src/components/culturalpropertydetail/CulturalPropertyButtons.tsx
@@ -6,11 +6,16 @@ import { useNavigate } from 'react-router-dom';
 import { AppState } from '../../store';
 import { CulturalPropertyData } from '../../types/culturalpropertytype';
 
-interface IsTrue {
-  isTrue: boolean;
+interface Coords {
+  latitude: number | undefined;
+  longitude: number | undefined;
 }
 
-export default function CulturalPropertyButtons({ isTrue }: IsTrue) {
+interface Props {
+  coords: Coords;
+}
+
+export default function CulturalPropertyButtons({ coords }: Props) {
   // 페이지 이동
   const culturalPropertydata = useSelector<
     AppState,
@@ -18,21 +23,23 @@ export default function CulturalPropertyButtons({ isTrue }: IsTrue) {
   >(({ culturalProperty }) => culturalProperty.value);
   const navigate = useNavigate();
   const goGame = () => {
+    console.log(coords.latitude, coords.longitude);
+
     // (window as any).Android.showGame(
     //   `${localStorage.getItem('accesstoken')}
     //   ${localStorage.getItem('culturalPropertyId')}`,
     // );
-    if (isTrue) {
-      (window as any).Android.showGame(
-        `${localStorage.getItem('accesstoken')}
-        ${localStorage.getItem('culturalPropertyId')}`,
-      );
-    } else {
-      Swal.fire({
-        text: '문화재에서 너무 먼 거리입니다',
-        confirmButtonColor: 'rgb(0, 170, 255)',
-      });
-    }
+    //   if (isDistance <= 50) {
+    //     (window as any).Android.showGame(
+    //       `${localStorage.getItem('accesstoken')}
+    //       ${localStorage.getItem('culturalPropertyId')}`,
+    //     );
+    //   } else {
+    //     Swal.fire({
+    //       text: '문화재 반경 50m 이내로 접근해주세요.',
+    //       confirmButtonColor: 'rgb(0, 170, 255)',
+    //     });
+    //   }
   };
   const goQuiz = () => {
     const region = culturalPropertydata?.result.culturalProperty.address;
