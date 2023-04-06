a83757a33b9a70743635f1e54e7dc780d5deb5fd
최지성 <thsrhzn4@naver.com>
2023-04-06 10:48:33 +0900

    feat: Spring batch 시간 설정
    Former-commit-id: f9630d3bd4ac5d8fa537d74d0898c5f53b080bf5

--- backend/src/main/java/com/ssafy/chorongddara/config/BatchScheduler.java
+++ backend/src/main/java/com/ssafy/chorongddara/config/BatchScheduler.java
@@ -24,7 +24,7 @@ public class BatchScheduler {
     @Autowired
     private BatchConfig batchConfig;
 
-    @Scheduled(cron = "0 40 10 * * *", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 0 11 * * *", zone = "Asia/Seoul")
     public void runJob() {
 
         // job parameter 설정
